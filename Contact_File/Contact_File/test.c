b3f1bee04c4cb5ff8373954497e0eb6ae24e389d
Ricky <2946677871@qq.com>
2021-01-29 11:08:08 +0800

    文件版通讯录

---
+++ Contact_File/Contact_File/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"contact.h"
+
+void menu()
+{
+	printf("*************************************************\n");
+	printf("***********1.add           2.del    *************\n");
+	printf("***********3.search        4.modify *************\n");
+	printf("***********5.show          6.sort   *************\n");
+	printf("***********7.save          0.exit   *************\n");
+	printf("*************************************************\n");
+}
+
+int main()
+{
+	int input = 0;
+	struct Contact con;
+	InitContact(&con);//初始化通讯录
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			AddContact(&con);
+			break;
+		case DELETE:
+			DelContact(&con);
+			break;
+		case SEARCH:
+			SearchContact(&con);
+			break;
+		case MODIFY:
+			ModifyContact(&con);
+			break;
+		case SHOW:
+			ShowContact(&con);
+			break;
+		case SORT:
+			//SortContact(&con);
+			break;
+		case EXIT:
+			SaveContact(&con);
+			DestroyContact(&con);
+			printf("退出通讯录。\n");
+			break;
+		case SAVE:
+			SaveContact(&con);
+			break;
+		default:
+			printf("选择错误!\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
